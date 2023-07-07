53cc640ec963edc96b3f962aea184129f910f4b8
GineTik <densheva18072003@gmail.com>
2023-07-07 15:11:03 +0300

    Added IUrlHelper extension

---
+++ Todoist/Helpers/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todoist.Helpers.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string? ActionLink<TConroller>(this IUrlHelper helper, string action)
+            where TConroller : Controller
+        {
+            var controllerName = nameof(TConroller);
+            var controllerNameLenght = controllerName.Length - "Controller".Length;
+            var controllerNameWithoutControllerWord = nameof(TConroller).Substring(0, controllerNameLenght);
+
+            return helper.ActionLink(action, controllerNameWithoutControllerWord);
+        }
+    }
+}
